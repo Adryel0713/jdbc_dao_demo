4c6ecd4167b78140f91b7c09d5f553db20823210
Adryel0713 <clauber.adriel@gmail.com>
2024-12-07 07:46:20 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,14 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department dp = new Department(1, "Adryel");
-		System.out.println(dp);
+		Seller s = new Seller(1, "Adryel", "adryel@gmail.com", new Date(), new Department(1, "Adryel"));
+		System.out.println(s);
 	}
 }
