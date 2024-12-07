3602453b478bd8ea69bad39c693f6e21f0f42e35
Adryel0713 <clauber.adriel@gmail.com>
2024-12-07 08:09:50 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,13 +2,18 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Seller s = new Seller(1, "Adryel", "adryel@gmail.com", new Date(), new Department(1, "Adryel"));
-		System.out.println(s);
+		Seller seller = new Seller(1, "Adryel", "adryel@gmail.com", new Date(), new Department(1, "Adryel"));
+
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+
+		System.out.println(seller);
 	}
 }
